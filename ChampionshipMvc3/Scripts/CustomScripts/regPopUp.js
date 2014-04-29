af0f15410a844ae2f7840bd9c77c8598b648937d
Zlatinsz <zlatin_sz@yahoo.com>
2014-04-29 21:10:04 +0300

    Fixed the problem with the javascript file

--- ChampionshipMvc3/Scripts/CustomScripts/regPopUp.js
+++ ChampionshipMvc3/Scripts/CustomScripts/regPopUp.js
@@ -1,25 +1,5 @@
 ﻿$(document).ready(function () {
-    $("#regPlayer").on('click', function () {
-        var url = $("#regPlayerModal").data('url');
-
-        $.get(url, function (data) {
-            $("#playerContainer").html(data);
-        });
-    });
-
-    $("#regTeam").on('click', function () {
-        var url = $("#regTeamModal").data('url');
-        $.get(url, function (data) {
-            $("#teamContainer").html(data);
-        });
-    });
-
-    var isIE11 = !!navigator.userAgent.match(/Trident.*rv\:11\./);
-
-    if (isIE11) {
-        $(".buttonInput").css('top', '-4px');
-        $(".buttonInput").css('right', '52px');
-    }else if(jQuery.browser.msie){
+if(jQuery.browser.msie){
         $(".buttonInput").css('top', '-4px');
         $(".buttonInput").css('right', '52px');
     }else if (jQuery.browser.mozilla){
